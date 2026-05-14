d6417d914633705d836b3979c6c979758a974aca
ms-hze21 <hazeldato-on@isufst.edu.ph>
2026-05-14 08:56:40 +0800

    feat: add landing page and update route to use it

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use App\Http\Controllers\Student\QuizController as StudentQuizController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('landing');
 });
 
 Route::get('/dashboard', [DashboardController::class, 'index'])
